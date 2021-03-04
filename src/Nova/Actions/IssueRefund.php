0b338331db8a0efedecbeb3f09c568a22a4d6c59
Drew Roberts <github@drewroberts.com>
2021-03-04 13:47:00 -0500

    Try resolving psalm issue

--- src/Nova/Actions/IssueRefund.php
+++ src/Nova/Actions/IssueRefund.php
@@ -24,6 +24,7 @@ class IssueRefund extends Action
      */
     public function handle(ActionFields $fields, Collection $models)
     {
+        /** @psalm-suppress TooManyArguments */
         $models->each(function ($model) use ($fields) {
             $model->issue();
             $model->notifyCustomer();
