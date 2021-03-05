945ef0a2af74cada80827db97eabe59d500fdb3a
wolfrednicolas <wolfrednicolas@users.noreply.github.com>
2021-03-05 00:35:34 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,8 +8,8 @@ use Laravel\Nova\NovaCoreServiceProvider;
 use Spatie\Permission\PermissionServiceProvider;
 use Tipoff\Authorization\AuthorizationServiceProvider;
 use Tipoff\Refunds\RefundsServiceProvider;
-use Tipoff\Support\SupportServiceProvider;
 use Tipoff\Refunds\Tests\Support\Providers\NovaPackageServiceProvider;
+use Tipoff\Support\SupportServiceProvider;
 use Tipoff\TestSupport\BaseTestCase;
 
 class TestCase extends BaseTestCase
