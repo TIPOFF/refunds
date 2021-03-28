bb56b9bc3abe7f2af5ef6e2657b029d4d022ba7d
Wolfred Montilla <wolfrednicolas@gmail.com>
2021-03-28 14:38:53 -0400

    add properly test for nova resources

--- tests/Feature/Nova/RefundResourceTest.php
+++ tests/Feature/Nova/RefundResourceTest.php
@@ -7,34 +7,118 @@ namespace Tipoff\Refunds\Tests\Feature\Nova;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tipoff\Refunds\Models\Refund;
 use Tipoff\Refunds\Tests\TestCase;
+use Tipoff\Authorization\Models\User;
 
 class RefundResourceTest extends TestCase
 {
-    use DatabaseTransactions;
+use DatabaseTransactions;
 
-    /** @test */
-    public function index()
+    private const NOVA_ROUTE = 'nova-api/refunds';
+
+    /**
+     * @dataProvider dataProviderForIndexByRole
+     * @test
+     */
+    public function index_by_role(?string $role, bool $hasAccess, bool $canIndex)
     {
+        $user = User::factory()->create();
+        if ($role) {
+            $user->assignRole($role);
+        }
+
+        $this->actingAs($user);
+
         Refund::factory()->count(4)->create();
 
-        $this->actingAs(self::createPermissionedUser('view refunds', true));
+        $response = $this->getJson(self::NOVA_ROUTE)
+            ->assertStatus($hasAccess ? 200 : 403);
 
-        $response = $this->getJson('nova-api/refunds')
-            ->assertOk();
-
-        $this->assertCount(4, $response->json('resources'));
+        if ($hasAccess) {
+            $this->assertCount($canIndex ? 4 : 0, $response->json('resources'));
+        }
     }
 
-    /** @test */
-    public function show()
+    public function dataProviderForIndexByRole()
     {
-        $refund = Refund::factory()->create();
-
-        $this->actingAs(self::createPermissionedUser('view refunds', true));
-
-        $response = $this->getJson("nova-api/refunds/{$refund->id}")
-            ->assertOk();
-
-        $this->assertEquals($refund->id, $response->json('resource.id.value'));
+        return [
+            'Admin' => ['Admin', true, true],
+            'Owner' => ['Owner', true, true],
+            'Executive' => ['Executive', true, true],
+            'Staff' => ['Staff', true, true],
+            'Former Staff' => ['Former Staff', false, false],
+            'Customer' => ['Customer', false, false],
+            'No Role' => [null, false, false],
+        ];
     }
+
+    /**
+     * @dataProvider dataProviderForShowByRole
+     * @test
+     */
+    public function show_by_role(?string $role, bool $hasAccess, bool $canView)
+    {
+        $user = User::factory()->create();
+        if ($role) {
+            $user->assignRole($role);
+        }
+        $this->actingAs($user);
+        $model = Refund::factory()->create();
+
+        $response = $this->getJson(self::NOVA_ROUTE . "/{$model->id}")
+            ->assertStatus($hasAccess ? 200 : 403);
+
+        if ($hasAccess && $canView) {
+            $this->assertEquals($model->id, $response->json('resource.id.value'));
+        }
+    }
+
+    public function dataProviderForShowByRole()
+    {
+        return [
+            'Admin' => ['Admin', true, true],
+            'Owner' => ['Owner', true, true],
+            'Executive' => ['Executive', true, true],
+            'Staff' => ['Staff', true, true],
+            'Former Staff' => ['Former Staff', false, false],
+            'Customer' => ['Customer', false, false],
+            'No Role' => [null, false, false],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForDeleteByRole
+     * @test
+     */
+    public function delete_by_role(?string $role, bool $hasAccess, bool $canDelete)
+    {
+        $user = User::factory()->create();
+        if ($role) {
+            $user->assignRole($role);
+        }
+        $this->actingAs($user);
+
+        $model = Refund::factory()->create();
+
+        // Request never fails
+        $this->deleteJson(self::NOVA_ROUTE . "?resources[]={$model->id}")
+            ->assertStatus($hasAccess ? 200 : 403);
+
+        // But deletion will only occur if user has permissions
+        $this->assertDatabaseCount('refunds', $canDelete ? 0 : 1);
+    }
+
+    public function dataProviderForDeleteByRole()
+    {
+        return [
+            'Admin' => ['Admin', true, false],
+            'Owner' => ['Owner', true, false],
+            'Executive' => ['Executive', true, false],
+            'Staff' => ['Staff', true, false],
+            'Former Staff' => ['Former Staff', false, false],
+            'Customer' => ['Customer', false, false],
+            'No Role' => [null, false, false],
+        ];
+    }
+
+
 }
